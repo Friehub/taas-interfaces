1c53ec9272819eec0cbfb22f0147e5b7fb742f08
Naija-geek <onlineweb.spport@gmail.com>
2026-02-26 22:14:31 +0100

    ci: configure github actions for package registry publishing

--- dist/TruthPoint.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * Data Point - v3.0 Foundation
- *
- * Wraps adapter data with rich metadata for lineage tracking,
- * quality metrics, and SLA monitoring.
- */
-Object.defineProperty(exports, "__esModule", { value: true });
--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './TruthData.js';
-export * from './DataSource.js';
-export * from './TruthPoint.js';
-export * from './SovereignAdapter.js';
-export * from './GenericRestAdapter.js';
